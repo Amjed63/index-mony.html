c5f9929fef5331b0faaad2f2be217d52e9ea71d2
Amjed63 <145359317+Amjed63@users.noreply.github.com>
2025-02-09 14:24:07 +0200

    Update scripts.js

--- Js/scripts.js
+++ Js/scripts.js
@@ -12,3 +12,43 @@ function showPage(pageId) {
     const activePage = document.getElementById(pageId);
     activePage.classList.add('active');
 }
+
+
+// modal box for images 
+   document.addEventListener("DOMContentLoaded", function() {
+        const modalImage1 = document.getElementById("modalImage1");
+        const modalVideo = document.getElementById("modalVideo");
+        const modalImage2 = document.getElementById("modalImage2");
+
+        document.querySelectorAll(".open-modal").forEach(button => {
+            button.addEventListener("click", function() {
+                const image1Src = this.getAttribute("data-image1");
+                const videoSrc = this.getAttribute("data-video");
+                const image2Src = this.getAttribute("data-image2");
+
+                // تحديث الصورة الأولى
+                if (image1Src) {
+                    modalImage1.src = image1Src;
+                    modalImage1.style.display = "block";
+                } else {
+                    modalImage1.style.display = "none";
+                }
+
+                // تحديث الفيديو
+                if (videoSrc) {
+                    modalVideo.src = videoSrc;
+                    modalVideo.style.display = "block";
+                } else {
+                    modalVideo.style.display = "none";
+                }
+
+                // تحديث الصورة الثانية
+                if (image2Src) {
+                    modalImage2.src = image2Src;
+                    modalImage2.style.display = "block";
+                } else {
+                    modalImage2.style.display = "none";
+                }
+            });
+        });
+    });
