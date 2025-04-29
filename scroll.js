6b3a6f33013995f6363a65597ff3f7b70e691e45
Amjed AbduRabu <145359317+Amjed63@users.noreply.github.com>
2025-04-29 05:42:32 +0300

    Add files via upload

---
+++ scroll.js
@@ -0,0 +1,16 @@
+let btn = document.getElementById('scro');
+
+onscroll= function(){
+    if(scrollY >=260){
+        btn.style.display= 'block';
+    }else{
+        btn.style.display= 'none';
+    }
+}
+btn.onclick= function(){
+    scroll({
+        left:0,
+        top:0,
+        behavior:'smooth'
+    })
+}
