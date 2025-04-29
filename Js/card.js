7f8e9d8596a6c0e21c73d95c60034fe49c2609c3
Amjed AbduRabu <145359317+Amjed63@users.noreply.github.com>
2025-04-29 11:28:10 +0300

    Update card.js

--- Js/card.js
+++ Js/card.js
@@ -1,6 +1,7 @@
-function openModal(e){let t=document.getElementById(e),l=t.querySelector("img").src,n=l.substring(l.lastIndexOf("/")+1),a=t.querySelector(".card-title").innerText,o=t.querySelector(".card-text").innerText,r=`مرحبًا، أرغب في حجز هذا العنصر:
+function openModal(e){let t=document.getElementById(e),l=t.querySelector("img").src,n=l.substring(l.lastIndexOf("/")+1),a=t.querySelector(".card-title").innerText,o=t.querySelector(".card-text").innerText,b=t.querySelector(".shape").innerText,r=`مرحبًا، أرغب في حجز هذا العنصر:
 
   العنوان: ${a}
-  الوصف: ${o}
+  الشكل:${b}
+  رقم السجاف: ${o}
   
-  اسم الصورة: ${n}`;document.getElementById("whatsapp-link").href=`https://wa.me/+967770833307?text=${encodeURIComponent(r)}`,document.getElementById("facebook-link").href="https://www.facebook.com/AlShohaiter",document.getElementById("instagram-link").href="https://www.instagram.com/seham_alhomaidi/",document.getElementById("call-link").href="tel:+967770833307",document.getElementById("myModal").style.display="flex"}function closeModal(){document.getElementById("myModal").style.display="none"}
+`;document.getElementById("whatsapp-link").href=`https://wa.me/+967770833307?text=${encodeURIComponent(r)}`,document.getElementById("facebook-link").href="https://www.facebook.com/AlShohaiter",document.getElementById("instagram-link").href="https://www.instagram.com/seham_alhomaidi/",document.getElementById("call-link").href="tel:+967770833307",document.getElementById("myModal").style.display="flex"}function closeModal(){document.getElementById("myModal").style.display="none"}
